99c745723475a2755abe75dc98a670d37ee073eb
Hitesh Kumar Saini <57103779+hit7sh@users.noreply.github.com>
2020-06-17 18:49:25 +0530

    Update bfs_snakes_and_ladder.cpp

--- bfs_snakes_and_ladder.cpp
+++ bfs_snakes_and_ladder.cpp
@@ -15,26 +15,19 @@ int main(){
         queue<int>q;
         q.push(1);
         vector<bool>visited(101);
-        vector<int>dist(101, INT_MAX);
-        dist[1]=0;
-        int flag = 0;
-
+        vector<int>dist(101);
+        dist[100]=-1;
         while(!q.empty()){
             int p = q.front();
             q.pop();
             visited[p]=true;
             for(int c=0,i = p+1; i<=p+6 && i<=100; ++i){
                 if(M.count(i) && !visited[M[i]])
-                    q.push(M[i]), dist[M[i]]=min(dist[p]+1, dist[M[i]]),  c+= M[i]<i;
+                    q.push(M[i]), dist[M[i]]=dist[p]+1, visited[M[i]]=true;
                 else if(!visited[i] && !M.count(i))
-                    q.push(i), dist[i]=min(dist[p]+1, dist[i]);         
-                if(c==6)
-                    flag = -1;
+                    q .push(i), dist[i]=dist[p]+1, visited[i]=true;
             }
         }
-        if(flag)
-            cout << flag <<endl; 
-        else
-            cout << dist[100] << endl;
+        cout << dist[100] << endl;
     }
 }
